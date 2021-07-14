4cf978e8e7a7a53a9f5eb8a0edb6c8b6885d7b85
lvshaoping007 <1244849816@qq.com>
2021-07-14 23:48:42 +0800

    oasis-ext

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import * as oasis from '@oasisprotocol/client';
 import * as oasisRT from '@oasisprotocol/client-rt';
 
 import * as oasisExt from '@oasisprotocol/client-ext-utils';
+let grpc = ""
 
+const oasisClient = new oasis.client.NodeInternal("grpc")
 
 const haveInterActive = false
 const extPath = haveInterActive ? '/oasis-xu-frame.html?test_noninteractive=1' : undefined;
@@ -14,6 +16,12 @@ function toBase64(/** @type {Uint8Array} */ u8) {
   return btoa(String.fromCharCode.apply(null, u8));
 }
 
+function hex2uint(hex) {
+  return new Uint8Array(Buffer.from(hex, 'hex'))
+}
+function uint2hex(uint) {
+  return Buffer.from(uint).toString('hex')
+}
 
 let connection
 let account
@@ -27,10 +35,13 @@ export const playground = (async function () {
   // 获取签名者
   // 打包交易
   // 4，签名
-  const extension_url = "chrome-extension://blgopabeahlgefobchbgbkekajmbnfmh"
 
-  // ID：blgopabeahlgefobchbgbkekajmbnfmh
-  // ID：fdkfkdobkkgngljecckfaeiabkinnnij
+  let extensionId = "fdkfkdobkkgngljecckfaeiabkinnnij"
+
+  const extension_url = "chrome-extension://" + extensionId
+
+  // blgopabeahlgefobchbgbkekajmbnfmh   ext
+  // fdkfkdobkkgngljecckfaeiabkinnnij     my ext
   const onboardButton = document.getElementById('connectButton')
   const getAccountsButton = document.getElementById('getAccounts')
   const accountsResults = document.getElementById('getAccountsResult')
@@ -42,7 +53,7 @@ export const playground = (async function () {
       // alert("请先安装oasis-extension-wallet")
       onboardButton.innerText = 'Onboarding in progress'
       connection = await oasisExt.connection.connect(extension_url, extPath);
-      console.log('connection===',connection)
+      console.log('connection===', connection)
       if (connection) {
         onboardButton.innerText = 'Connected'
         onboardButton.disabled = true
@@ -60,10 +71,15 @@ export const playground = (async function () {
    */
   getAccountsButton.onclick = async () => {
     if (connection) {
-      const keys = await oasisExt.keys.list(connection);
-      console.log(keys)
-      account = keys
-      accountsResults.innerHTML = keys || 'Not able to get accounts'
+      const keys = await oasisExt.keys.list(connection).catch(err => err);
+      let result = keys
+      if (Array.isArray(keys)) {
+        result = keys
+      } else {
+        result = keys.error
+      }
+      account = result
+      accountsResults.innerHTML = result || 'Not able to get accounts'
     }
   }
 
@@ -74,120 +90,143 @@ export const playground = (async function () {
   const sendButton = document.getElementById('sendButton')
   const sendAmountInput = document.getElementById('sendAmountInput')
   const receiveAddressInput = document.getElementById('receiveAddressInput')
+
+
+  const sendNonceInput = document.getElementById('sendNonceInput')
+  const sendFeeGasInput = document.getElementById('sendFeeGasInput')
+  const sendFeeAmountInput = document.getElementById('sendFeeAmountInput')
   const sendResultDisplay = document.getElementById('sendResultDisplay')
 
   /**
    * send transfer
    */
   sendButton.onclick = async () => {
-    let from = account && account.length > 0 ? account[0] : ""
-    console.log("web---sendButton===from",from)
-    console.log("web---sendButton===connection",connection)
-    const signer = await oasisExt.signature.ExtContextSigner.request(connection, from);
-    console.log('web---got signer');
+    try {
 
-    //第四步 公钥
-    const publicKey = signer.public();
-    console.log('web---public key base64', toBase64(publicKey));
-    console.log(
-      'web---address bech32',
-      oasis.staking.addressToBech32(await oasis.staking.addressFromPublicKey(publicKey)),
-    );
 
-    //第五步 获取收款地址
-    const dst = oasis.signature.NaclSigner.fromRandom('this key is not important');
+      // 设置转账金额
+      // 设置收款金额
+      // 设置nonce
+      // 设置feeGas
+      // 设置feeAmount
 
-    const tw = oasis.staking
-      .transferWrapper()
-      .setNonce(101n)
-      .setFeeAmount(oasis.quantity.fromBigInt(102n))
-      .setFeeGas(103n)
-      .setBody({
-        to: await oasis.staking.addressFromPublicKey(dst.public()),
-        amount: oasis.quantity.fromBigInt(104n),
-      });
-    console.log('web---requesting signature');
-    // 第七步 签名
-    await tw.sign(signer, 'fake-chain-context-for-testing');
-    console.log('web---got signature');
-    console.log('web---signature base64', toBase64(tw.signedTransaction.signature.signature));
-    // let signResult = await window.oasis.signTransaction({
-    //   amount: sendAmountInput.value,
-    //   from: from,
-    //   to: receiveAddressInput.value,
-    // })
-    // sendResultDisplay.innerHTML = signResult.error || signResult.hash
+      let from = account && account.length > 0 ? account[0] : ""
+
+      const signer = await oasisExt.signature.ExtContextSigner.request(connection, from).catch(err => err);
+      console.log('web---got signer', signer);
+      if (signer.error) {
+        alert(signer.error)
+        return
+      }
+      //第四步 公钥
+      const publicKey = signer.public();
+      console.log('web---public key base64', toBase64(publicKey));
+      console.log(
+        'web---address bech32',
+        oasis.staking.addressToBech32(await oasis.staking.addressFromPublicKey(publicKey)),
+      );
+
+      //第五步 获取收款地址
+
+      let sendAmount = sendAmountInput.value || 2
+      sendAmount = oasis.quantity.fromBigInt(sendAmount)
+
+      let receiveAddress = receiveAddressInput.value || "oasis1qzaa7s3df8ztgdryn8u8zdsc8zx0drqsa5eynmam"
+      receiveAddress = await oasis.staking.addressFromBech32(receiveAddress)
+
+      let sendNonce = sendNonceInput.value || 43
+
+      let sendFeeGas = sendFeeGasInput.value || 3000
+      sendFeeGas = oasis.quantity.fromBigInt(sendFeeGas)
+
+      let sendFeeAmount = sendFeeAmountInput.value || 0
+      sendFeeAmount = oasis.quantity.fromBigInt(sendFeeAmount)
+
+
+
+      const tw = oasis.staking
+        .transferWrapper()
+        .setNonce(sendNonce)
+        .setFeeAmount(sendFeeAmount)
+        .setFeeGas(sendFeeGas)
+        .setBody({
+          to: receiveAddress,
+          amount: sendAmount,
+        });
+      await tw.sign(signer, 'fake-chain-context-for-testing');
+      let signature = hex2uint(tw.signedTransaction.signature.signature)
+      
+      await tw.submit(oasisClient);
+      
+      let hash = await tw.hash()
+
+      sendResultDisplay.innerHTML = hash || ''
+    } catch (error) {
+      sendResultDisplay.innerHTML = error || ''
+    }
   }
 
 
+  /**
+   * add Escrow
+   */
+
+  /**
+   * add escrow
+   */
+  const addEscrowButton = document.getElementById('addEscrowButton')
+  const addEscrowAmountInput = document.getElementById('addEscrowAmountInput')
+  const vaildatorAddressInput = document.getElementById('vaildatorAddressInput')
+  const addEscrowResultDisplay = document.getElementById('addEscrowResultDisplay')
+
+  const stakeNonceInput = document.getElementById('stakeNonceInput')
+  const stakeFeeGasInput = document.getElementById('stakeFeeGasInput')
+  const stakeFeeAmountInput = document.getElementById('stakeFeeAmountInput')
+
+  addEscrowButton.onclick = async () => {
+    let from = account && account.length > 0 ? account[0] : ""
+    console.log("web---sendButton===from", from)
+    console.log("web---sendButton===connection", connection)
+
+    const signer = await oasisExt.signature.ExtContextSigner.request(connection, from).catch(err => err);
+    console.log('web---got signer', signer);
 
 
+    let addEscrowAmount = addEscrowAmountInput.value || 2
+    addEscrowAmount = oasis.quantity.fromBigInt(addEscrowAmount)
 
-  /*
-  
-    console.log('connecting');
-    const extension_url = "chrome-extension://blgopabeahlgefobchbgbkekajmbnfmh"
-    // 第一步 连接账户
-    const connection = await oasisExt.connection.connect(extension_url, extPath);
-    console.log('connected==', connection);
-  
-    //第二步 获取所有的key
-    console.log('listing keys');
-    const keys = await oasisExt.keys.list(connection);
-    console.log('listed keys');
-    console.log('keys', keys);
-  
-    // 第三步 获取签名者
-    console.log('requesting signer');
-    const signer = await oasisExt.signature.ExtContextSigner.request(connection, keys[0].which);
-    console.log('got signer');
-    //第四步 公钥
-    const publicKey = signer.public();
-    console.log('public key base64', toBase64(publicKey));
-    console.log(
-      'address bech32',
-      oasis.staking.addressToBech32(await oasis.staking.addressFromPublicKey(publicKey)),
-    );
+    let vaildatorAddress = vaildatorAddressInput.value || "oasis1qzaa7s3df8ztgdryn8u8zdsc8zx0drqsa5eynmam"
+    vaildatorAddress = await oasis.staking.addressFromBech32(vaildatorAddress)
+
+    let stakeNonce = stakeNonceInput.value || 43
+
+    let stakeFeeGas = stakeFeeGasInput.value || 3000
+    stakeFeeGas = oasis.quantity.fromBigInt(stakeFeeGas)
+
+    let stakeFeeAmount = stakeFeeAmountInput.value || 0
+    stakeFeeAmount = oasis.quantity.fromBigInt(stakeFeeAmount)
+
+
     //第五步 获取收款地址
     const dst = oasis.signature.NaclSigner.fromRandom('this key is not important');
-    
-    // 第六步 打包交易
+
+
     const tw = oasis.staking
-      .transferWrapper()
-      .setNonce(101n)
-      .setFeeAmount(oasis.quantity.fromBigInt(102n))
-      .setFeeGas(103n)
+      .addEscrowWrapper()
+      .setNonce(stakeNonce)
+      .setFeeAmount(stakeFeeAmount)
+      .setFeeGas(stakeFeeGas)
       .setBody({
-        to: await oasis.staking.addressFromPublicKey(dst.public()),
-        amount: oasis.quantity.fromBigInt(104n),
+        account: vaildatorAddress,
+        amount: addEscrowAmount,
       });
-    console.log('requesting signature');
     // 第七步 签名
-    await tw.sign(signer, 'fake-chain-context-for-testing');
-    console.log('got signature');
-    console.log('signature base64', toBase64(tw.signedTransaction.signature.signature));
-  
-    
-    // const rtw = new oasisRT.accounts.Wrapper(oasis.misc.fromString('fake-runtime-id-for-testing'))
-    //   .callTransfer()
-    //   .setBody({
-    //     to: await oasis.staking.addressFromPublicKey(dst.public()),
-    //     amount: [oasis.quantity.fromBigInt(105n), oasis.misc.fromString('TEST')],
-    //   })
-    //   .setSignerInfo([
-    //     {
-    //       address_spec: { signature: { ed25519: publicKey } },
-    //       nonce: 106n,
-    //     },
-    //   ])
-    //   .setFeeAmount([oasis.quantity.fromBigInt(107n), oasisRT.token.NATIVE_DENOMINATION])
-    //   .setFeeGas(108n);
-    // console.log('requesting signature');
-    // await rtw.sign([signer], 'fake-chain-context-for-testing');
-    // console.log('got signature');
-    // console.log('signature base64', toBase64(rtw.unverifiedTransaction[1][0].signature));
-  
-    */
+    let res = await tw.sign(signer, 'fake-chain-context-for-testing');
+    let signature = hex2uint(tw.signedTransaction.signature.signature)
+    let base64Sign = toBase64(signature)
+    let hash = await tw.hash()
+    addEscrowResultDisplay.innerHTML = hash || ''
+  }
 })();
 
 playground.catch((e) => {
