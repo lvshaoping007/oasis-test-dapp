f4ca0ec05c4d7a4d23b573f5fbb56469d3f160d6
lvshaoping007 <1244849816@qq.com>
2021-07-26 23:04:38 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@
 // @ts-check
 
 import * as oasis from '@oasisprotocol/client';
-import * as oasisRT from '@oasisprotocol/client-rt';
-
 import * as oasisExt from '@oasisprotocol/client-ext-utils';
-let grpc = ""
-
-const oasisClient = new oasis.client.NodeInternal("grpc")
 
 const haveInterActive = false
 const extPath = haveInterActive ? '/oasis-xu-frame.html?test_noninteractive=1' : undefined;
@@ -23,9 +18,40 @@ function uint2hex(uint) {
   return Buffer.from(uint).toString('hex')
 }
 
+function watchKeys(conn, handleNewKeys) {
+  console.log('watchKeys======2',conn);
+  if(!conn){
+    return
+  }
+  console.log('watchKeys======3');
+  let lastRequested = 0;
+  oasisExt.keys.setKeysChangeHandler(conn, (_) => {
+    console.log('watchKeys======4');
+      const requestSeq = ++lastRequested;
+      oasisExt.keys
+          .list(conn)
+          .then((keys) => {
+              console.log('watchKeys======5',keys);
+              if (requestSeq !== lastRequested) return;
+              console.log('watchKeys======6',keys);
+              handleNewKeys(keys);
+          })
+          .catch((e) => {
+              console.error(e);
+          });
+  });
+}
+
+function getOasisClient(){
+  const oasisClient = new oasis.client.NodeInternal("https://grpc-testnet.oasisscan.com")
+  return oasisClient
+}
+
 let connection
-let account
-export const playground = (async function () {
+let account={}
+try {
+  
+const playground = (async function () {
 
   // 1,点击connect 去连接账户
   // 2，如果有connect ，则可以点击获取账户
@@ -37,7 +63,8 @@ export const playground = (async function () {
   // 4，签名
 
   
-  let extensionId = "aeiciliacehpifhikhkgkmohihocgain"//"fdkfkdobkkgngljecckfaeiabkinnnij"
+  let extensionId = "fdkfkdobkkgngljecckfaeiabkinnnij"
+  // "aeiciliacehpifhikhkgkmohihocgain"//"fdkfkdobkkgngljecckfaeiabkinnnij"
 
   const extension_url = "chrome-extension://" + extensionId
 
@@ -56,6 +83,12 @@ export const playground = (async function () {
       connection = await oasisExt.connection.connect(extension_url, extPath);
       console.log('connection===', connection)
       if (connection) {
+        console.log('watchKeys======0',connection);
+        watchKeys(connection, (newKeys) => {
+          console.log('watchKeys======1',newKeys);
+          console.log('keys change',toBase64(newKeys[0].metadata.public_key));
+        });
+
         onboardButton.innerText = 'Connected'
         onboardButton.disabled = true
       } else {
@@ -67,28 +100,38 @@ export const playground = (async function () {
       onboardButton.disabled = true
     }
   }
+  const publicKeyToAddress = async (publicKey) => {
+    let address 
+    let public_key = publicKey?.metadata?.public_key || ""
+    if(public_key){
+      console.log('publicKey',uint2hex(public_key))
+      const data = await oasis.staking.addressFromPublicKey(public_key)
+      address =  oasis.staking.addressToBech32(data)
+      account.public_key = public_key
+      account.address = address
+    }
+    accountsResults.innerHTML = address || 'Not able to get accounts'
+}
+
   /**
    * get account
    */
   getAccountsButton.onclick = async () => {
     if (connection) {
-      const keys = await oasisExt.keys.list(connection).catch(err => err);
-      let result = keys
-      if (Array.isArray(keys)) {
-        result = keys
+      const keys = await oasisExt.keys.list(connection)
+      .catch(err => err);
+      let result
+      if (keys.length > 0) {
+        result = keys[0]
+        result = publicKeyToAddress(result)
       } else {
         result = keys.error
+        accountsResults.innerHTML = result || 'Not able to get accounts'
       }
-      account = result
-      accountsResults.innerHTML = result || 'Not able to get accounts'
     }
   }
 
-
-
-  // transfer
-
-  const sendButton = document.getElementById('sendButton')
+  const sendButton2 = document.getElementById('sendButton') 
   const sendAmountInput = document.getElementById('sendAmountInput')
   const receiveAddressInput = document.getElementById('receiveAddressInput')
 
@@ -101,19 +144,22 @@ export const playground = (async function () {
   /**
    * send transfer
    */
-  sendButton.onclick = async () => {
+   sendButton2.onclick = async () => {
+    console.log('sendButton=====0',account);
     try {
-
 
       // 设置转账金额
       // 设置收款金额
       // 设置nonce
       // 设置feeGas
       // 设置feeAmount
-
-      let from = account && account.length > 0 ? account[0] : ""
-
-      const signer = await oasisExt.signature.ExtContextSigner.request(connection, from).catch(err => err);
+      console.log('sendButton=====0',account);
+      let from = account && account.address  ? account.address : ""
+      const KEY_ID = 'sample-singleton';
+      console.log('sendButton=====1',from);
+      console.log('sendButton=====1-1',connection);
+      const signer = await oasisExt.signature.ExtContextSigner.request(connection,from).catch(err => err);
+      console.log('sendButton=====2',signer);
       console.log('web---got signer', signer);
       if (signer.error) {
         alert(signer.error)
@@ -121,6 +167,8 @@ export const playground = (async function () {
       }
       //第四步 公钥
       const publicKey = signer.public();
+
+      console.log('sendButton=====3',publicKey);
       console.log('web---public key base64', toBase64(publicKey));
       console.log(
         'web---address bech32',
@@ -143,8 +191,6 @@ export const playground = (async function () {
       let sendFeeAmount = sendFeeAmountInput.value || 0
       sendFeeAmount = oasis.quantity.fromBigInt(sendFeeAmount)
 
-
-
       const tw = oasis.staking
         .transferWrapper()
         .setNonce(sendNonce)
@@ -155,14 +201,30 @@ export const playground = (async function () {
           amount: sendAmount,
         });
       await tw.sign(signer, 'fake-chain-context-for-testing');
-      let signature = hex2uint(tw.signedTransaction.signature.signature)
       
-      await tw.submit(oasisClient);
+      console.log('sendButton=====5',tw.signedTransaction.signature.signature);
+      console.log('sendButton=====6',uint2hex(tw.signedTransaction.signature.signature));
+      
+      console.log('sendButton=====7',tw.signedTransaction.untrusted_raw_value);
+      console.log('sendButton=====8',uint2hex(tw.signedTransaction.untrusted_raw_value));
+
+      console.log('sendButton=====9',oasis.misc.toHex(oasis.misc.toCBOR(tw.signedTransaction)));
+
+
+      
+      try {
+          await tw.submit(getOasisClient());
+      } catch (e) {
+          console.error('submit failed', e);
+          throw e;
+      }
       
       let hash = await tw.hash()
+      console.log('sendButton=====6',hash);
 
       sendResultDisplay.innerHTML = hash || ''
     } catch (error) {
+      console.log('sendButton=====error',error);
       sendResultDisplay.innerHTML = error || ''
     }
   }
@@ -233,3 +295,6 @@ export const playground = (async function () {
 playground.catch((e) => {
   console.error(e);
 });
+} catch (error) {
+  console.log("error===error",error);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,85 @@ const CopyPlugin = require('copy-webpack-plugin')
 
 const DIST = path.resolve(__dirname, 'dist')
 
-module.exports = {
-  devtool: 'eval-source-map',
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: DIST,
-    publicPath: DIST,
-  },
-  devServer: {
-    contentBase: DIST,
-    port: 9011,
-    writeToDisk: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+// module.exports = {
+  // devtool: 'eval-source-map',//1
+  // mode: 'development',//1
+  // entry: './src/index.js',//1
+  // output: {//1
+    // filename: 'bundle.js',
+    // path: DIST,
+    // publicPath: DIST,
+  // },
+//   devServer: {//   11
+//     contentBase: DIST,
+//     port: 9011,
+//     writeToDisk: true,
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+//     new CopyPlugin({
+//       patterns: [
+//         {
+//           flatten: true,
+//           from: './src/*',
+//           globOptions: {
+//             ignore: ['**/*.js'],
+//           },
+//         },
+//       ],
+//     }),
+//   ],
+// }
 
-    // for build scripts
-    new CopyPlugin({
-      patterns: [
-        {
-          flatten: true,
-          from: './src/*',
-          globOptions: {
-            ignore: ['**/*.js'],
-          },
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    resolve: { 
+      fallback: { 
+        stream: require.resolve('stream-browserify') ,
+      }
+     },
+    // devtool: false,
+    devtool: 'eval-source-map',
+    entry: './src/index.js',
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new CopyPlugin({
+          patterns: [
+            {
+              flatten: true,
+              from: './src/*',
+              // globOptions: {
+              //   ignore: ['**/*.js'],
+              // },
+            },
+          ],
+        }),
+    ],
+    output: {
+        library: {
+            name: 'playground',
+            type: 'window',
+            export: 'playground',
         },
-      ],
-    }),
-  ],
-}
+        filename: 'bundle.js',
+        path: DIST,
+        // publicPath: DIST,
+    },
+    // In tests, we serve the extension files as a plain old website.
+    devServer: {
+        // contentBase: DIST,
+        // devMiddleware: {
+        //     publicPath: DIST,
+        // },
+        port: 9011,
+        // writeToDisk: true,
+        static: DIST,
+    },
+};
